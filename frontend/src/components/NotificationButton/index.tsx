1f9297f8a1562340a57672eefaed94b4bb815ae7
Mateus Alves Conceição <mateus.alvesc02@gmail.com>
2022-09-18 12:51:51 -0300

    Toast

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { toast } from 'react-toastify';
 import icon from '../../assets/img/notification-icon.svg';
 import { BASE_URL } from '../../utils/request';
 
@@ -8,15 +9,14 @@ type Props = {
     saleId: number;
 }
 
+function handleClick(id : number) {
+    axios(`${BASE_URL}/sales/${id}/notification`)
+        .then(response => {
+            toast.info("SMS enviado com sucesso");
+        });
+}
+
 function NotificationButton( {saleId} : Props ) {
-
-    function handleClick(id : number) {
-        axios(`${BASE_URL}/sales/${id}/notification`)
-            .then(response => {
-                console.log("SUCESSO!");
-            });
-    }
-
     return (
         <>
             <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
